0cc26562b801f2e557e840802d8c0f61de5b89b8
Scott McCormack <scott.mccormack@outlook.com>
2017-10-29 21:23:24 +0800

    Refactored to suit revised project structure

--- utils/feature_extractor.py
+++ utils/feature_extractor.py
@@ -6,35 +6,41 @@ import re
 # run this script with no arguments in the folder with all the cpickle files.
 # will create a file called data.txt if it doesn't exist already, otherwise will overwrite it
 
+data_dir = os.path.join('..', 'data', 'DCASE')
+meta_path = os.path.join(data_dir, 'meta.txt')
+dirs_to_eval = ['evaluator', 'feature_extractor']
+
+# Load the meta file and parse the file
 files = {}
-with open('meta.txt', 'r') as metaFile:
+with open(meta_path, 'r') as metaFile:
     for line in metaFile:
         line = line.split('\t')[0:2]
         name = re.search(r'/(.+)\.', line[0]).group()[1:-1]
         clss = line[1]
         files[name] = clss
 
-allFeats = ''
-for filename in os.listdir('.'):
-    if filename.endswith('.cpickle'):
-        # get file name
-        nameId = re.sub(r'\.cpickle', '', filename)
+output = '' # Output to be passed to the
+count = 0 # Count the number of processed cpickle files
 
-        data = pickle.load(open(filename, 'rb'))
+# Walk through the parent directory and load all the .cpickle files that are found
+for dirName, subdirList, fileList in os.walk(data_dir):
+    for filename in fileList:
+        if filename.endswith('.cpickle'):
+            count += 1
+            nameId = re.sub(r'\.cpickle', '', filename)  # get file name
+            pickle_data = pickle.load(open(os.path.join(dirName, filename), 'rb'))
+            feat_data = np.array(pickle_data['feat'][0])
 
-        data = np.array(data['feat'][0])
+            instanceFeatures = ''
+            for i in feat_data.transpose():
+                for j in np.array_split(i, 4):
+                    instanceFeatures += str(np.mean(j)) + '\t'
+                    instanceFeatures += str(np.std(j)) + '\t'
 
-        instanceFeatures = ''
-        for feature in range(0, 40):
-            featureSlices = data[:, feature]
-            instanceFeatures += str(np.mean(featureSlices)) + '\t'
-            instanceFeatures += str(np.std(featureSlices)) + '\t'
-            if feature == 39:
-                instanceFeatures += files[nameId] + '\n'
+            output += instanceFeatures + files[nameId] + '\n'
 
-            # print(instanceFeatures)
-    allFeats += instanceFeatures
-print(allFeats)
+# Write the file out to the parent folder where it sourced the cpickle files from
+filename_out = os.path.join(data_dir, 'data_{0}_all.txt'.format(count))
 
-with open('data.txt', 'w') as saveFile:
-    saveFile.write(allFeats)
+with open(filename_out, 'w') as saveFile:
+    saveFile.write(output)
